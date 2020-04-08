9b604953a72135216d477c6fc06145c384db2b75
Bojke <danijelbojcic@gmail.com>
2020-04-08 08:56:20 +0200

    simplify vlc player

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 
 const VLCPlayerControl = {
   VLCPlayer: require('./VLCPlayer').default,
-  VlCPlayerView: require('./playerView/index').default,
-  VlCPlayerViewByMethod: require('./playerViewByMethod/index').default,
-  VlcSimplePlayer: require('./playerViewByMethod/index').default,
 };
 
 module.exports = VLCPlayerControl;
